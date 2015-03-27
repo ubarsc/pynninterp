1beb8c96dfa703c9aff1938275be4073700464b0
Pete Bunting <petebunting@mac.com>
2015-03-27 14:10:56 +1000

    update with a version which builds now need to add python interface.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup, Extension
 
 pynninterp = Extension('pynninterp',
-                sources = ['delaunay.c', 'delaunay.h', 'hash.c', 'hash.h', 'istack.c', 'istack.h', 'lpi.c', 'minell.c', 'minell.h', 'nan.h', 'nn.h', 'nn_internal.h', 'nnai.c', 'nncommon-vulnerable.c', 'nncommon.c', 'nnpi.c', 'preader.c', 'preader.h', 'triangle.c', 'triangle.h', 'version.h'])
+                define_macros = [('MAJOR_VERSION', '1'),
+                                 ('MINOR_VERSION', '0')],
+                sources = ['src/delaunay.c', 'src/hash.c', 'src/istack.c', 'src/lpi.c', 'src/minell.c', 'src/nnai.c', 'src/nncommon-vulnerable.c', 'src/nncommon.c', 'src/nnpi.c', 'src/preader.c', 'src/triangle.c'])
 
 setup (name = 'PyNNInterp',
        version = '1.0',
        description = 'This is a package for running a natural neighbour interpolation from python.',
+       author = 'John Armston, Pete Bunting, Neil Flood, Sam Gillingham',
+       author_email = 'petebunting@mac.com',
        ext_modules = [pynninterp])

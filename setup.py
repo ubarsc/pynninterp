663bf03c2db4ea5f25e465d5f91b02e540faf78b
Pete Bunting <petebunting@mac.com>
2015-03-30 16:08:00 +1000

    Update to pynninterp so it is now working and includes two functions 1) for nn and 2) for linear (TIN?) interpolators.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup, Extension
 import numpy
 
-pynninterp = Extension('PyNNInterp',
+pynninterp = Extension('pynninterp',
                 define_macros = [('MAJOR_VERSION', '1'),
                                  ('MINOR_VERSION', '0')],
                 sources = ['src/performNNInterp.c', 'src/delaunay.c', 'src/hash.c', 'src/istack.c', 'src/lpi.c', 'src/minell.c', 'src/nnai.c', 'src/nncommon-vulnerable.c', 'src/nncommon.c', 'src/nnpi.c', 'src/preader.c', 'src/triangle.c'],
                 include_dirs=[numpy.get_include()])
 
-setup (name = 'PyNNInterp',
+setup (name = 'pynninterp',
        version = '1.0',
        description = 'This is a package for running a natural neighbour interpolation from python.',
        author = 'John Armston, Pete Bunting, Neil Flood, Sam Gillingham',
--- src/performNNInterp.c
+++ src/performNNInterp.c
@@ -1,9 +1,8 @@
 /*
- * performNNInterp.cpp
+ *  performNNInterp.c
  *
- *  Created by Pete Bunting on 27/03/2015.
  *
- * This file is part of PyNNInterp
+ * This file is part of PyLidar
  * Copyright (C) 2015 John Armston, Pete Bunting, Neil Flood, Sam Gillingham
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,24 +17,16 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
- 
-#include <Python.h>
-#include <numpy/arrayobject.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <float.h>
+
 #include <math.h>
-#include <errno.h>
 
-/*
-#include "nan.h"
-#include "minell.h"
+#include <Python.h>
+#include "numpy/arrayobject.h"
+
 #include "nn.h"
-*/
 
 /* An exception object for this module */
 /* created in the init function */
@@ -51,155 +42,296 @@ struct PyNNInterpState
 static struct PyNNInterpState _state;
 #endif
 
-
-int notDoubleVector1D(PyArrayObject *vec)
+static PyObject *pynninterp_naturalneighbour(PyObject *self, PyObject *args)
 {
-    if (vec->descr->type_num != NPY_DOUBLE || vec->nd != 1)
+    //std::cout.precision(12);
+    PyObject *pXVals, *pYVals, *pZVals, *pXGrid, *pYGrid;
+    PyObject *pOutArray;
+    
+    if( !PyArg_ParseTuple(args, "OOOOO:NaturalNeighbour", &pXVals, &pYVals, &pZVals, &pXGrid, &pYGrid))
+        return NULL;
+    
+    if( !PyArray_Check(pXVals) || !PyArray_Check(pYVals) || !PyArray_Check(pZVals) || !PyArray_Check(pXGrid) || !PyArray_Check(pYGrid) )
     {
-        PyErr_SetString(PyExc_ValueError, "In notDoubleVector1D: array must be of type Float and 1 dimensional (n).");
-        return 1;
+        PyErr_SetString(GETSTATE(self)->error, "All arguments must be numpy arrays");
+        return NULL;
     }
-    return 0;
+    
+    // Check dimensions match
+    if( (PyArray_DIM(pXVals, 0) != PyArray_DIM(pYVals, 0)) | (PyArray_DIM(pXVals, 0) != PyArray_DIM(pZVals, 0)))
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Training X, Y and Z arrays must all be of the same length");
+        return NULL;
+    }
+    
+    if( (PyArray_DIM(pXGrid, 0) != PyArray_DIM(pYGrid, 0)) | (PyArray_DIM(pXGrid, 1) != PyArray_DIM(pYGrid, 1)))
+    {
+        PyErr_SetString(GETSTATE(self)->error, "X and Y grids must have the same dimensions");
+        return NULL;
+    }
+    
+    // TODO: check types ok
+    
+    npy_intp nRows = PyArray_DIM(pXGrid, 0);
+    npy_intp nCols = PyArray_DIM(pXGrid, 1);
+    
+    npy_intp nVals = PyArray_DIM(pXVals, 0);
+    
+    // Create output
+    pOutArray = PyArray_EMPTY(2, PyArray_DIMS(pXGrid), NPY_DOUBLE, 0);
+    if( pOutArray == NULL )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Failed to create array");
+        return NULL;
+    }
+    
+    if( PyArray_DIM(pXVals, 0) < 3 )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Not enough points, need at least 3.");
+        return NULL;
+    }
+    
+    if( nVals < 100 )
+    {
+        // check that these small number of points aren't all within a line
+        double meanX = 0;
+        double meanY = 0;
+        
+        double varX = 0;
+        double varY = 0;
+        
+        for(size_t i = 0; i < nVals; ++i)
+        {
+            meanX += *((double*)PyArray_GETPTR1(pXVals, i));
+            meanY += *((double*)PyArray_GETPTR1(pYVals, i));
+        }
+        
+        meanX = meanX / nVals;
+        meanY = meanY / nVals;
+        
+        for(size_t i = 0; i < nVals; ++i)
+        {
+            varX += *((double*)PyArray_GETPTR1(pXVals, i)) - meanX;
+            varY += *((double*)PyArray_GETPTR1(pYVals, i)) - meanY;
+        }
+        
+        varX = fabs(varX / nVals);
+        varY = fabs(varY / nVals);
+        
+        if((varX < 4) || (varY < 4))
+        {
+            PyErr_SetString(GETSTATE(self)->error, "Points are all within a line.");
+            return NULL;
+        }
+    }
+    
+    // BUILD POINT ARRAYS
+    point *inPts = malloc(nVals * sizeof(point));
+    for(npy_intp i = 0; i < nVals; ++i)
+    {
+        inPts[i].x = *((double*)PyArray_GETPTR1(pXVals, i));
+        inPts[i].y = *((double*)PyArray_GETPTR1(pYVals, i));
+        inPts[i].z = *((double*)PyArray_GETPTR1(pZVals, i));
+    }
+    
+    npy_intp nPtsOutGrid = nRows * nCols;
+    npy_intp idx = 0;
+    point *gPts = malloc(nPtsOutGrid * sizeof(point));
+    for(npy_intp i  = 0; i < nRows; ++i)
+    {
+        for(npy_intp j = 0; j < nCols; ++j)
+        {
+            gPts[idx].x = *((double*)PyArray_GETPTR2(pXGrid, i, j));
+            gPts[idx].y = *((double*)PyArray_GETPTR2(pYGrid, i, j));
+            gPts[idx].z = 0.0;
+            ++idx;
+        }
+    }
+    
+    double minWeight = 0.0;
+    nnpi_interpolate_points(nVals, inPts, minWeight, nPtsOutGrid, gPts);
+    
+    free(inPts);
+    
+    
+    // POPULATE GRID
+    idx = 0;
+    for(npy_intp i  = 0; i < nRows; ++i)
+    {
+        for(npy_intp j = 0; j < nCols; ++j)
+        {
+            *((double*)PyArray_GETPTR2(pOutArray, i, j)) = gPts[idx++].z;
+        }
+    }
+    
+    free(gPts);
+    
+    return pOutArray;
 }
 
-int notDoubleVector2D(PyArrayObject *vec)
+
+static PyObject *pynninterp_linear(PyObject *self, PyObject *args)
 {
-    if (vec->descr->type_num != NPY_DOUBLE || vec->nd != 2)
+    //std::cout.precision(12);
+    PyObject *pXVals, *pYVals, *pZVals, *pXGrid, *pYGrid;
+    PyObject *pOutArray;
+    
+    if( !PyArg_ParseTuple(args, "OOOOO:Linear", &pXVals, &pYVals, &pZVals, &pXGrid, &pYGrid))
+        return NULL;
+    
+    if( !PyArray_Check(pXVals) || !PyArray_Check(pYVals) || !PyArray_Check(pZVals) || !PyArray_Check(pXGrid) || !PyArray_Check(pYGrid) )
     {
-        PyErr_SetString(PyExc_ValueError, "In notDoubleVector2D: array must be of type Float and 2 dimensional (n).");
-        return 1;
+        PyErr_SetString(GETSTATE(self)->error, "All arguments must be numpy arrays");
+        return NULL;
     }
-    return 0;
+    
+    // Check dimensions match
+    if( (PyArray_DIM(pXVals, 0) != PyArray_DIM(pYVals, 0)) | (PyArray_DIM(pXVals, 0) != PyArray_DIM(pZVals, 0)))
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Training X, Y and Z arrays must all be of the same length");
+        return NULL;
+    }
+    
+    if( (PyArray_DIM(pXGrid, 0) != PyArray_DIM(pYGrid, 0)) | (PyArray_DIM(pXGrid, 1) != PyArray_DIM(pYGrid, 1)))
+    {
+        PyErr_SetString(GETSTATE(self)->error, "X and Y grids must have the same dimensions");
+        return NULL;
+    }
+    
+    // TODO: check types ok
+    
+    npy_intp nRows = PyArray_DIM(pXGrid, 0);
+    npy_intp nCols = PyArray_DIM(pXGrid, 1);
+    
+    npy_intp nVals = PyArray_DIM(pXVals, 0);
+    
+    // Create output
+    pOutArray = PyArray_EMPTY(2, PyArray_DIMS(pXGrid), NPY_DOUBLE, 0);
+    if( pOutArray == NULL )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Failed to create array");
+        return NULL;
+    }
+    
+    if( PyArray_DIM(pXVals, 0) < 3 )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Not enough points, need at least 3.");
+        return NULL;
+    }
+    
+    if( nVals < 100 )
+    {
+        // check that these small number of points aren't all within a line
+        double meanX = 0;
+        double meanY = 0;
+        
+        double varX = 0;
+        double varY = 0;
+        
+        for(size_t i = 0; i < nVals; ++i)
+        {
+            meanX += *((double*)PyArray_GETPTR1(pXVals, i));
+            meanY += *((double*)PyArray_GETPTR1(pYVals, i));
+        }
+        
+        meanX = meanX / nVals;
+        meanY = meanY / nVals;
+        
+        for(size_t i = 0; i < nVals; ++i)
+        {
+            varX += *((double*)PyArray_GETPTR1(pXVals, i)) - meanX;
+            varY += *((double*)PyArray_GETPTR1(pYVals, i)) - meanY;
+        }
+        
+        varX = fabs(varX / nVals);
+        varY = fabs(varY / nVals);
+        
+        if((varX < 4) || (varY < 4))
+        {
+            PyErr_SetString(GETSTATE(self)->error, "Points are all within a line.");
+            return NULL;
+        }
+    }
+    
+    // BUILD POINT ARRAYS
+    point *inPts = malloc(nVals * sizeof(point));
+    for(npy_intp i = 0; i < nVals; ++i)
+    {
+        inPts[i].x = *((double*)PyArray_GETPTR1(pXVals, i));
+        inPts[i].y = *((double*)PyArray_GETPTR1(pYVals, i));
+        inPts[i].z = *((double*)PyArray_GETPTR1(pZVals, i));
+    }
+    
+    npy_intp nPtsOutGrid = nRows * nCols;
+    npy_intp idx = 0;
+    point *gPts = malloc(nPtsOutGrid * sizeof(point));
+    for(npy_intp i  = 0; i < nRows; ++i)
+    {
+        for(npy_intp j = 0; j < nCols; ++j)
+        {
+            gPts[idx].x = *((double*)PyArray_GETPTR2(pXGrid, i, j));
+            gPts[idx].y = *((double*)PyArray_GETPTR2(pYGrid, i, j));
+            gPts[idx].z = 0.0;
+            ++idx;
+        }
+    }
+    
+    lpi_interpolate_points(nVals, inPts, nPtsOutGrid, gPts);
+    
+    free(inPts);
+    
+    
+    // POPULATE GRID
+    idx = 0;
+    for(npy_intp i  = 0; i < nRows; ++i)
+    {
+        for(npy_intp j = 0; j < nCols; ++j)
+        {
+            *((double*)PyArray_GETPTR2(pOutArray, i, j)) = gPts[idx++].z;
+        }
+    }
+    
+    free(gPts);
+    
+    return pOutArray;
 }
 
-static PyObject *PyNNInterp_interpNNGrid(PyObject *self, PyObject *args, PyObject *keywds)
-{
-    printf ("Entered of function.\n");
-    PyArrayObject *inX;
-    PyArrayObject *inY;
-    PyArrayObject *inValues;
-    PyArrayObject *xGrid;
-    PyArrayObject *yGrid;
-    printf ("Created Variables.\n");
-    
-    static char *kwlist[] = {"x", "y", "values", "xGrid", "yGrid", NULL};
-    
-    if(!PyArg_ParseTupleAndKeywords(args, keywds, "OOOOO:interpNNGrid", kwlist, &inX, &inY, &inValues, &xGrid, &yGrid))
-    {
-        return NULL;
-    }
-    
-    if(inX == NULL || notDoubleVector1D(inX))
-    {
-        PyErr_SetString(GETSTATE(self)->error, "inX must be type double (numpy.float64) and 1D");
-        return NULL;
-    }
-    size_t nInX = inX->dimensions[0];
-    
-    if(inY == NULL || notDoubleVector1D(inY))
-    {
-        PyErr_SetString(GETSTATE(self)->error, "inY must be type double (numpy.float64) and 1D");
-        return NULL;
-    }
-    size_t nInY = inY->dimensions[0];
-    
-    if(inValues == NULL || notDoubleVector1D(inValues))
-    {
-        PyErr_SetString(GETSTATE(self)->error, "inValues must be type double (numpy.float64) and 1D");
-        return NULL;
-    }
-    size_t nInVals = inValues->dimensions[0];
-    
-    if( (nInX != nInY) | (nInX != nInVals) )
-    {
-        PyErr_SetString(GETSTATE(self)->error, "inX, inY and inValues must be the same length");
-        return NULL;
-    }
-    
-    if(xGrid == NULL || notDoubleVector2D(xGrid))
-    {
-        PyErr_SetString(GETSTATE(self)->error, "xGrid must be type double (numpy.float64) and 2D");
-        return NULL;
-    }
-    size_t nXGridRows = xGrid->dimensions[0];
-    size_t nXGridCols = xGrid->dimensions[1];
-    
-    if(yGrid == NULL || notDoubleVector2D(yGrid))
-    {
-        PyErr_SetString(GETSTATE(self)->error, "yGrid must be type double (numpy.float64) and 2D");
-        return NULL;
-    }
-    size_t nYGridRows = yGrid->dimensions[0];
-    size_t nYGridCols = yGrid->dimensions[1];
-    
-    printf ("X [%f, %f]\n", nXGridRows, nXGridCols);
-    printf ("Y [%f, %f]\n", nYGridRows, nYGridCols);
-
-    if( (nXGridRows != nYGridRows) | (nXGridCols != nYGridCols) )
-    {
-        PyErr_SetString(GETSTATE(self)->error, "xGrid and yGrid must be the same size.");
-        return NULL;
-    }
-    
-    double *inXVals = (double *) inX->data;
-    double *inYVals = (double *) inY->data;
-    double *inVals = (double *) inValues->data;
-    double *xGridVals = (double*) xGrid->data;
-    double *yGridVals = (double*) yGrid->data;
-    
-    for(int i = 0; i < nInX; ++i)
-    {
-        printf ("%d [%f, %f, %f]\n", i, inXVals[i], inYVals[i], inVals[i]);
-    }
-    
-    
-    
-    
-    
-    
-    
-
-    printf ("Hello World.\n");
-    
-    /*
-    PyObject *outGrid;
-    
-    outGrid = PyArray_NewLikeArray(xGrid, NPY_ANYORDER, NULL, 0);
-    if (outGrid == NULL)
-    {
-        return NULL;
-    }
-    
-    return outGrid;
-     */
-    Py_RETURN_NONE;
-}
-
-
+/* Our list of functions in this module*/
 static PyMethodDef PyNNInterpMethods[] = {
-    {"interpNNGrid", (PyCFunction)PyNNInterp_interpNNGrid, METH_VARARGS | METH_KEYWORDS,
-        "interpNNGrid(x=numpy.array, y=numpy.array, values=numpy.array, xGrid=numpy.array, yGrid=numpy.array)\n"
-        "Interpolates values  for x,y locations on a grid defined by xGrid and yGrid.\n"
-        "Where:\n"
-        "\n"
-        "* x is a numpy array\n"
-        "* y is a numpy array\n"
-        "* values is a numpy array\n"
-        "* xGrid is a numpy array\n"
-        "* yGrid is a numpy array\n"
-        "\n"},
-        {NULL}        /* Sentinel */
+    {"NaturalNeighbour", pynninterp_naturalneighbour, METH_VARARGS,
+        "Perform Natural Neighbour Interpolation\n"
+        "call signature: arr = NaturalNeighbour(xvals, yvals, zvals, xgrid, ygrid)\n"
+        "where:\n"
+        "  xvals is a 1d array of the x values of the points\n"
+        "  yvals is a 1d array of the y values of the points\n"
+        "  zvals is a 1d array of the z values of the points\n"
+        "xvals, yvals and zvals should have the same length\n"
+        "  xgrid is a 2d array of x coordinates to interpolate at\n"
+        "  ygrid is a 2d array of y coordinates to interpolate at\n"
+        "xgrid and xgrid must be the same shape"},
+    {"Linear", pynninterp_linear, METH_VARARGS,
+        "Perform Linear (TIN) Interpolation\n"
+        "call signature: arr = Linear(xvals, yvals, zvals, xgrid, ygrid)\n"
+        "where:\n"
+        "  xvals is a 1d array of the x values of the points\n"
+        "  yvals is a 1d array of the y values of the points\n"
+        "  zvals is a 1d array of the z values of the points\n"
+        "xvals, yvals and zvals should have the same length\n"
+        "  xgrid is a 2d array of x coordinates to interpolate at\n"
+        "  ygrid is a 2d array of y coordinates to interpolate at\n"
+        "xgrid and xgrid must be the same shape"},
+    {NULL}        /* Sentinel */
 };
 
-
 #if PY_MAJOR_VERSION >= 3
-static int PyNNInterp_traverse(PyObject *m, visitproc visit, void *arg)
+
+static int pynninterp_traverse(PyObject *m, visitproc visit, void *arg)
 {
     Py_VISIT(GETSTATE(m)->error);
     return 0;
 }
 
-static int PyNNInterp_clear(PyObject *m)
+static int pynninterp_clear(PyObject *m)
 {
     Py_CLEAR(GETSTATE(m)->error);
     return 0;
@@ -207,49 +339,53 @@ static int PyNNInterp_clear(PyObject *m)
 
 static struct PyModuleDef moduledef = {
     PyModuleDef_HEAD_INIT,
-    "_PyNNInterp",
+    "pynninterp",
     NULL,
     sizeof(struct PyNNInterpState),
     PyNNInterpMethods,
     NULL,
-    PyNNInterp_traverse,
-    PyNNInterp_clear,
+    pynninterp_traverse,
+    pynninterp_clear,
     NULL
 };
 
 #define INITERROR return NULL
 
 PyMODINIT_FUNC
-PyInit_PyNNInterp(void)
+PyInit_pynninterp(void)
 
 #else
 #define INITERROR return
 
 PyMODINIT_FUNC
-init_PyNNInterp(void)
+initpynninterp(void)
 #endif
 {
+    PyObject *pModule;
+    struct PyNNInterpState *state;
+    
+    /* initialize the numpy stuff */
+    import_array();
+    
 #if PY_MAJOR_VERSION >= 3
-    PyObject *pModule = PyModule_Create(&moduledef);
+    pModule = PyModule_Create(&moduledef);
 #else
-    PyObject *pModule = Py_InitModule("_PyNNInterp", PyNNInterpMethods);
+    pModule = Py_InitModule("pynninterp", PyNNInterpMethods);
 #endif
     if( pModule == NULL )
         INITERROR;
     
-    struct PyNNInterpState *state = GETSTATE(pModule);
+    state = GETSTATE(pModule);
     
-    // Create and add our exception type
-    state->error = PyErr_NewException("_PyNNInterp.error", NULL, NULL);
+    /* Create and add our exception type */
+    state->error = PyErr_NewException("pynn.error", NULL, NULL);
     if( state->error == NULL )
     {
         Py_DECREF(pModule);
         INITERROR;
     }
     
-    
 #if PY_MAJOR_VERSION >= 3
     return pModule;
 #endif
 }
-
